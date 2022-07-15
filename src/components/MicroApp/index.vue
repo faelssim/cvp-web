266725560fba55c734f47607aac60533f1cf76bc
wangkun <wangkun@baiwang.com>
2022-07-15 21:14:04 +0800

    feat: add microapp

--- src/components/MicroApp/index.vue
+++ src/components/MicroApp/index.vue
@@ -1,14 +1,15 @@
 <!--
  * @Author: wangkun
  * @Date: 2022-07-15 16:29:10
- * @LastEditTime: 2022-07-15 16:34:00
+ * @LastEditTime: 2022-07-15 17:38:27
  * @LastEditors: wangkun
  * @Description: 
 -->
 <template>
-    <h1>MicroApp</h1>
+    <div id="micro-app"></div>
 </template>
 <script>
+import { loadMicroApp } from 'qiankun'
 export default {
     mounted () {
         console.log(this.$route)
